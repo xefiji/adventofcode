c712d51b2473df35a0008347ea5a32ea5e4ede2c
FX Echappe <francois-xavier.echappe@ankorstore.com>
2022-11-25 18:07:03 +0100

    2019 day2

--- 2019/day2/main.py
+++ 2019/day2/main.py
@@ -1,20 +1,28 @@
 import sys
 
 def part1():
-    change = lambda x, i: 12 if i == 1 else 2 if i == 2 else x
-    lines = [change(x, i) for i, x in enumerate(list(map(int,open("input.csv").read().split(","))))]
-    pos = 0
-    while True:
-        if lines[pos] == 99:
-            return lines[0]
-        elif lines[pos] == 2:
-            lines[lines[pos+3]] = lines[lines[pos+1]] * lines[lines[pos+2]]
-        elif lines[pos] == 1:
-            lines[lines[pos+3]] = lines[lines[pos+1]] + lines[lines[pos+2]]
-        pos +=4    
+    return compute(12, 2)
 
 def part2():
-    return None
+    for a in range(100):
+        for b in range(100):
+            if compute(a, b) == 19690720:
+                return 100*a+b
+
+
+def compute(a, b):
+    change = lambda x, i: a if i == 1 else b if i == 2 else x
+    int_code = [change(x, i) for i, x in enumerate(list(map(int,open("input.csv").read().split(","))))]
+    pos = 0    
+    while True:        
+        if pos >= len(int_code) or int_code[pos] == 99:            
+            return int_code[0]            
+        elif int_code[pos] == 2:
+            int_code[int_code[pos+3]] = int_code[int_code[pos+1]] * int_code[int_code[pos+2]]
+        elif int_code[pos] == 1:
+            int_code[int_code[pos+3]] = int_code[int_code[pos+1]] + int_code[int_code[pos+2]]
+        pos +=4
+
 
 print('result of part 1 is {}'.format(part1()))
 print('result of part 2 is {}'.format(part2()))
---
+++ 2019/day3/main.py
@@ -0,0 +1,10 @@
+import sys
+
+def part1():
+    return None
+
+def part2():
+    return None
+
+print('result of part 1 is {}'.format(part1()))
+print('result of part 2 is {}'.format(part2()))
